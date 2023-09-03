3f8c4e95d0e8b517ce22301f44d8c3a45ece11eb
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-09-03 15:15:32 +0530

    initial setup for plugin

---
+++ gutenberg-slideshow.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Gutenberg Slideshow
+ * Description:       It provides the users with a custom Gutenberg block that fetches the latest data from a WordPress site and display data as a slideshow.
+ * Version:           1.0
+ * Author:            Dhruvik Malaviya
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       caraousal-slider-block
+ 
+ */
+
+ if(!defined('ABSPATH')){
+    header("Location: /Gutenberg-Slideshow");
+    die("");
+ }
+
+ 
